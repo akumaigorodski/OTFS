20e85ba657de89ce07d112fa4b81fb3a5835dfcd
Anton <akumaigorodski@gmail.com>
2024-06-24 19:17:08 +0300

    WIP Trellis Encoded Modulation

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -3,7 +3,7 @@ package otfs
 import scodec.bits.ByteVector
 
 @main
-def main(): Unit = {
+def main: Unit = {
   val encoding4 = QAM(ModulationOrder.QAM4)
   val params = FrameParams(dopplerBins = 2, delayBins = 4, freqDelta = 15e3, freqCarrier = 4e9)
   val message = ByteVector.encodeUtf8("Hello, world!").getOrElse(ByteVector.empty)
